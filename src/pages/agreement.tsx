fc6a33f16798ba2d8cc433ffa6fee1bec11df686
Samiul Haque Sohan <s@shq.su>
2024-06-30 22:04:55 +0300

    update document submission checklists

--- src/pages/agreement.tsx
+++ src/pages/agreement.tsx
@@ -8,20 +8,20 @@ import { useNavigate } from 'react-router-dom';
 // };
 
 const Agreement: React.FC = () => {
-    const [checkedBoxes, setCheckedBoxes] = useState<any[]>([]);
+  const [checkedBoxes, setCheckedBoxes] = useState<any[]>([]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleCheckboxChange = (event:any) => {
-        const isChecked = event.target.checked;
-        const value = event.target.value;
-    
-        if (isChecked) {
-          setCheckedBoxes([...checkedBoxes, value]);
-        } else {
-          setCheckedBoxes(checkedBoxes.filter((box:any) => box !== value));
-        }
-      };
+  const handleCheckboxChange = (event: any) => {
+    const isChecked = event.target.checked;
+    const value = event.target.value;
+
+    if (isChecked) {
+      setCheckedBoxes([...checkedBoxes, value]);
+    } else {
+      setCheckedBoxes(checkedBoxes.filter((box: any) => box !== value));
+    }
+  };
 
   const handleBack = () => {
     navigate('/dashboard');
@@ -31,28 +31,46 @@ const Agreement: React.FC = () => {
     navigate('/file-submission');
   };
 
-  const allChecked = checkedBoxes.length === 5; 
-    
-    return(
-        <>
-        <Button className="mb-8" onClick={handleBack}>
+  const allChecked = checkedBoxes.length === 5;
+
+  return (
+    <>
+      <Button className="mb-8" onClick={handleBack}>
         Back
       </Button>
-        <div className='flex flex-col justify-center items-center'>
-
-        <div>Check all the checkbox to proceed.</div>
-        <div className='flex flex-col justify-center items-center space-y-8 h-96'>
-        <div className='flex flex-col space-y-8'>
-        <Checkbox onChange={handleCheckboxChange}>Checkbox</Checkbox>
-        <Checkbox onChange={handleCheckboxChange}>Checkbox</Checkbox>
-        <Checkbox onChange={handleCheckboxChange}>Checkbox</Checkbox>
-        <Checkbox onChange={handleCheckboxChange}>Checkbox</Checkbox>
-        <Checkbox onChange={handleCheckboxChange}>Checkbox</Checkbox>
+      <div className="flex flex-col justify-center items-center">
+        <div>
+          Before continuing a file, please make sure all the checklists are
+          ticked from your end.
         </div>
-        <Button disabled={!allChecked} onClick={handleFront} style={{ width: "150px"}}>Proceed</Button>
+        <div className="flex flex-col justify-center items-center space-y-8 h-96">
+          <div className="flex flex-col space-y-8">
+            <Checkbox onChange={handleCheckboxChange}>
+              SSC Marksheet + Certificate
+            </Checkbox>
+            <Checkbox onChange={handleCheckboxChange}>
+              HSC Marksheet + Certificate (Can be provisional)
+            </Checkbox>
+            <Checkbox onChange={handleCheckboxChange}>
+              Bachelors Certificates + marksheets (For Master's students only)
+            </Checkbox>
+            <Checkbox onChange={handleCheckboxChange}>
+              Attestation from Education board + Education Ministry + Foreign
+              Ministry (backside of the main copy and front side of the
+              photocopy (4 pieces))
+            </Checkbox>
+            <Checkbox onChange={handleCheckboxChange}>Valid Passport</Checkbox>
+          </div>
+          <Button
+            disabled={!allChecked}
+            onClick={handleFront}
+            style={{ width: '150px' }}
+          >
+            Proceed
+          </Button>
         </div>
-        </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 export default Agreement;
